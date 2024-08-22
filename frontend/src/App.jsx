d075e46096fee48e7a84a5686581e8a53b575ca5
StoicaDanielMarianGr3 <127768215+StoicaDanielMarianGr3@users.noreply.github.com>
2024-08-22 15:40:16 +0300

    comp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,11 +8,12 @@ import About from './pages/About'
 import Collection from './pages/Collection'
 import Home from './pages/Home'
 import Orders from './pages/Orders'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:pg-[9vw]'>
-      
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='collection' element={<Collection/>} /> 
