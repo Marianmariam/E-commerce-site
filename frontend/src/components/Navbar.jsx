7868758553ab7585658dda1007d49417c5be555d
StoicaDanielMarianGr3 <127768215+StoicaDanielMarianGr3@users.noreply.github.com>
2024-08-25 13:28:02 +0300

    added logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import {assets} from "../../ecommerce-app/assets/frontend_assets";
+import {assets} from '../assets/assets';
 
 
 const Navbar = () => {
   return (
     <div className='flex items-center jusitfy-between py-5 font-medium'>
       
-    <img src={frontend_assets.logo} className='w-36' alt="" />
+    <img src={assets.logo} className='w-36' alt="" />
 
     </div>
   )
