423e60899b18e9afeac6bef34cd7a3091d07a5ed
StoicaDanielMarianGr3 <127768215+StoicaDanielMarianGr3@users.noreply.github.com>
2024-08-25 15:17:03 +0300

    hover

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { assets } from '../assets/assets';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+
   return (
     <div className='flex items-center justify-between py-5 px-4 font-medium'>
       
@@ -30,13 +32,27 @@ const Navbar = () => {
       <div className='flex items-center gap-6'>
         <img src={assets.search_icon} className='w-6 cursor-pointer ml-4' alt="Search" />
         
-        <div className='group relative'>
-          <img src={assets.profile_icon} alt="Profile" />
+        <div 
+          className='relative'
+          onMouseEnter={() => setIsMenuVisible(true)}
+          onMouseLeave={() => setIsMenuVisible(false)}
+        >
+          <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="Profile" />
+          
+          {/* Dropdown Menu */}
+          {isMenuVisible && (
+            <div className='absolute right-0 mt-2 w-36 bg-slate-100 border border-gray-200 rounded-lg shadow-lg'>
+              <div className='flex flex-col gap-2 py-3 text-gray-500'>
+                <p className='cursor-pointer px-4 hover:text-black'>My Profile</p>
+                <p className='cursor-pointer px-4 hover:text-black'>Orders</p>
+                <p className='cursor-pointer px-4 hover:text-black'>Logout</p>
+              </div>
+            </div>
+          )}
         </div>
       </div>
-    </div>    
-  )
+    </div>
+  );
 }
 
-export default Navbar
-
+export default Navbar;
