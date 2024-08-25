e5465dc7c3c0b5c77f1189b90cde823c80b32090
Marianmariam <razvanbanescu452@gmail.com>
2024-08-25 14:23:54 +0300

    added profile icon

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,6 +1,8 @@
 import logo from './logo.png'
 import search_icon from './search_icon.png'
+import profile_icon from './profile_icon.png'
 export const assets={
     logo,
-    search_icon
+    search_icon,
+    profile_icon
 }
--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='flex-flex-col sm:flex-row border border-gray-400'> 
-      
+      {/* Left side */}
+      <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
+        <div className='text-[#414141]'>
+            
+
+        </div>
+      </div>
+
     </div>
   )
 }
